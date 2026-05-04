60d04fba7d1d5d85c127f946bca598d1845e992d
Tim Janepat <tim@janepat.com>
2026-05-04 08:25:28 +0700

    feat: add Dockerfile + standalone output for Coolify deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "db.aiceo.im" },
+      { protocol: "https", hostname: "i.ytimg.com" },
+      { protocol: "https", hostname: "img.youtube.com" },
+      { protocol: "https", hostname: "yt3.ggpht.com" },
+    ],
+  },
 };
 
 export default nextConfig;
